984b3ffa66e11e65ca4b7b5035a9ca1d75a30798
ChetanBatra72 <Chetanbat2001@gmail.com>
2023-09-02 15:06:14 +0530

    updated banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,11 +5,28 @@ import HeroBannerImage from '../assets/images/ban7.webp';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
-    </Typography>
+   <Typography color="#FF2625" fontWeight="600" fontSize="56px"> Welcome to Fitness Club</Typography>
+   <Typography
+  fontWeight={900}
+  sx={{
+    fontSize: { lg: '56px', xs: '20px' }, // Increased font size
+    lineHeight: '1.5', // Improved line height
+    textAlign: 'left', // left text
+    fontFamily:'roboto',
+    color: "blue",
+
+  }}
+  mb="20px" // Reduced bottom margin for better alignment
+  mt="30px"
+>
+   Sweat, Smile & Repeat💪
+  {/* Unleash your potential with our personalized fitness programs.
+  <br />
+  Sweat, smile, and repeat as you embark on your journey
+  <br />
+  to a healthier, happier you. */}
+</Typography>
+
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
     </Typography>
